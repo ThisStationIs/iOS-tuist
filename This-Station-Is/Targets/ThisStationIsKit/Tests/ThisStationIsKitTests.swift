5841235ec378df599f1b8829ce10fc301ef46e5e
simyo <hj.sim@muzlive.com>
2023-12-05 16:12:27 +0900

    [Feat] tuist: tuist로 프로젝트 생성

---
+++ This-Station-Is/Targets/ThisStationIsKit/Tests/ThisStationIsKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class ThisStationIsKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("ThisStationIsKit", "ThisStationIsKit")
+    }
+}
---
+++ This-Station-Is/Targets/ThisStationIsUI/Sources/ThisStationIsUI.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public final class ThisStationIsUI {
+    public static func hello() {
+        print("Hello, from your UI framework")
+    }
+}

8570ab1db6642d4cbf1f6773e50650925ffee8f6
simyo <hj.sim@muzlive.com>
2023-12-15 14:37:25 +0900

    [Feat] add tuist Templat(+scaffold)

---
+++ This-Station-Is/Tuist/Templates/Module+Templates.swift
@@ -0,0 +1,35 @@
+//
+//  Module+Templates.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Muzlive_Player on 2023/12/15.
+//
+
+import ProjectDescription
+
+let nameAttribute = Template.Attribute.required("name")
+let authorAttribute = Template.Attribute.optional("author", default: "unknown")
+
+let template = Template(
+    description: "This is \(nameAttribute) module. Author is \(authorAttribute)",
+    attributes: [
+        nameAttribute,
+        authorAttribute
+    ],
+    items: [
+        [
+            .file(path: "Projects/Modules/\(nameAttribute)/Samples/Sources/AppDelegate.swift", templatePath: "appdelegate.stencil"),
+            .file(path: "Projects/Modules/\(nameAttribute)/Samples/Resources/Assets.xcassets/contents.json", templatePath: "xcassets.stencil")
+        ],
+        [
+            .file(path: "Projects/Modules/\(nameAttribute)/Sources/\(nameAttribute).swift", templatePath: "sources.stencil"),
+            .string(path: "Projects/Modules/\(nameAttribute)/Tests/dummy.txt", contents: "_"),
+            .string(path: "Projects/Modules/\(nameAttribute)/Resources/dummy.txt", contents: "_")
+        ],
+        [
+            .file(path: "Projects/Modules/\(nameAttribute)/Project.swift", templatePath: "project.stencil")
+        ]
+    ].flatMap { $0 }
+)
+
+

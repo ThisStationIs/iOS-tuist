188c3782fdbd98d034cb4e4cb8ed6fd46ab7fc5e
simyo <hj.sim@muzlive.com>
2023-12-15 17:32:38 +0900

    [Fix] stencil 파일 및 템플릿 코드 수정

--- This-Station-Is/Project.swift
+++
@@ -1,9 +0,0 @@
-import ProjectDescription
-import ProjectDescriptionHelpers
-import MyPlugin
-
-let project = Project.makeApp(
-    name: "ThisStationIsTuist",
-    deploymentTarget: .iOS(targetVersion: "15.0", devices: [.ipad]),
-    infoPlist: .default
-)
--- This-Station-Is/Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ This-Station-Is/Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -1,7 +1,8 @@
 import ProjectDescription
 
 extension Project {
-    /// Helper function to create the Project for this ExampleApp
+    static let baseBundleId: String = "com.kkonmo"
+    
     public static func makeApp(
         name: String,
         platform: Platform = .iOS,
@@ -13,20 +14,7 @@ extension Project {
         
         let options: Options = .options()
         
-        let settings: Settings = .settings(
-            base: [:],
-            configurations: [
-                .debug(name: .debug, settings: [
-                    "CODE_SIGN_STYLE": "Automatic",
-                    "DEVELOPMENT_TEAM": "L8WBW3DU2X" // TODO: 수정
-                ]),
-                .release(name: .release, settings: [
-                    "CODE_SIGN_STYLE": "Automatic",
-                    "DEVELOPMENT_TEAM": "L8WBW3DU2X" // TODO: 수정
-                ])
-            ],
-            defaultSettings: .recommended
-        )
+        let settings: Settings = getSettings()
         
         let targets = makeTargets(
             name: name,
@@ -42,7 +30,7 @@ extension Project {
         
         return Project(
             name: name,
-            organizationName: "This-Station-Is", // TODO: 수정
+            organizationName: "Kkonmo", // TODO: 수정
             options: options,
             packages: packages,
             settings: settings,
@@ -50,17 +38,152 @@ extension Project {
             schemes: schemes
         )
     }
-}
-
-
-extension Project {
-    public static func makeTargets(
+    
+    private static func makeTargets(
         name: String,
         platform: Platform,
         deploymentTarget: DeploymentTarget,
         infoPlist: InfoPlist,
         dependencies: [TargetDependency]
     ) -> [Target] {
-        return []
+        let mainTarget = Target(
+            name: name,
+            platform: platform,
+            product: .app,
+            bundleId: baseBundleId + ".\(name)",
+            deploymentTarget: deploymentTarget,
+            sources: ["Sources/**"],
+            resources: ["Resources/**"],
+            dependencies: dependencies
+        )
+        
+        let testTarget = Target(
+            name: "\(name)Tests",
+            platform: platform,
+            product: .unitTests,
+            bundleId: baseBundleId + ".\(name)Tests",
+            deploymentTarget: deploymentTarget,
+            sources: ["Tests/**"],
+            dependencies: [
+                .target(name: name),
+                .xctest
+            ]
+        )
+        
+        return [mainTarget, testTarget]
+    }
+}
+
+extension Project {
+    public static func makeModule(
+        name: String,
+        platform: Platform = .iOS,
+        product: Product,
+        organizationName: String = "Kkonmo",
+        packages: [Package] = [],
+        deploymentTarget: DeploymentTarget,
+        dependencies: [TargetDependency] = [],
+        resources: ResourceFileElements? = nil,
+        baseSetting: [String: SettingValue] = [:],
+        infoPlist: InfoPlist = .default
+    ) -> Project {
+        let settings: Settings = getSettings()
+        
+        let schemes: [Scheme] = [
+            .makeScheme(name: name, .debug)
+        ]
+        
+        let targets: [Target] = makeModuleTargets(
+            name: name,
+            platform: platform,
+            product: product,
+            deploymentTarget: deploymentTarget,
+            infoPlist: infoPlist,
+            dependencies: dependencies,
+            resources: resources
+        )
+        
+        return Project(
+            name: name,
+            organizationName: organizationName,
+            packages: packages,
+            settings: settings,
+            targets: targets,
+            schemes: schemes
+        )
+    }
+    
+    private static func makeModuleTargets(
+        name: String,
+        platform: Platform,
+        product: Product,
+        deploymentTarget: DeploymentTarget,
+        infoPlist: InfoPlist,
+        dependencies: [TargetDependency],
+        resources: ResourceFileElements? = nil
+    ) -> [Target] {
+        let mainTarget = Target(
+            name: "\(name)",
+            platform: platform,
+            product: product,
+            bundleId: baseBundleId + ".\(name)",
+            deploymentTarget: deploymentTarget,
+            infoPlist: infoPlist,
+            sources: ["Sources/**"],
+            resources: resources,
+            dependencies: dependencies
+        )
+        
+        let testTarget = Target(
+            name: "\(name)Tests",
+            platform: platform,
+            product: product,
+            bundleId: baseBundleId + ".\(name)Tests",
+            deploymentTarget: deploymentTarget,
+            infoPlist: .default,
+            sources: ["Tests/**"],
+            dependencies: [
+                .target(name: name),
+                .xctest
+            ]
+        )
+        
+        let sampleTarget = Target(
+            name: "\(name)Sample",
+            platform: platform,
+            product: product,
+            bundleId: baseBundleId + ".\(name)Sample",
+            deploymentTarget: deploymentTarget,
+            infoPlist: infoPlist,
+            sources: ["Samples/**"],
+            resources: ["Samples/Resources/**"],
+            dependencies: [
+                .target(name: name)
+            ]
+        )
+        
+        return [mainTarget, testTarget, sampleTarget]
+    }
+}
+
+
+extension Project {
+    private static func getSettings(
+        codeSignStyle: SettingValue = "Automatic",
+        developmentTeam: SettingValue = ""
+    ) -> Settings {
+        return .settings(
+            base: [:],
+            configurations: [
+                .debug(
+                    name: .debug,
+                    settings: [
+                        "CODE_SIGN_STYLE": codeSignStyle,
+                        "DEVELOPMENT_TEAM": developmentTeam // TODO: 수정
+                    ]
+                )
+            ],
+            defaultSettings: .recommended
+        )
     }
 }

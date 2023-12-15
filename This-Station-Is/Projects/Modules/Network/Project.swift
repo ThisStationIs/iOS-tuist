1fc2272804c55de03ea0389b300d1fdb1f70603e
simyo <hj.sim@muzlive.com>
2023-12-15 17:33:46 +0900

    [Feat] Network: scaffold 를 통해 프로젝트 생성

---
+++ This-Station-Is/Projects/Modules/Network/Project.swift
@@ -0,0 +1,15 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project.makeModule(
+    name: "Network",
+    product: .staticLibrary,
+    deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone]),
+    dependencies: [],
+    infoPlist: configureInfoPlist()
+)
+
+private func configureInfoPlist(merging other: [String: InfoPlist.Value] = [:]) -> InfoPlist {
+    var extendedPlist: [String: InfoPlist.Value] = [:]
+    return InfoPlist.extendingDefault(with: extendedPlist)
+}

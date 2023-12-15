b7faed85f533329b5c117abda940a1baeb7319fb
simyo <hj.sim@muzlive.com>
2023-12-15 23:34:38 +0900

    [Feat] Network: 모듈 코드 작성

---
+++ This-Station-Is/Projects/Modules/Network/Sources/RequestResponsable.swift
@@ -0,0 +1,42 @@
+//
+//  RequestResponsable.swift
+//  Network
+//
+//  Created by Muzlive_Player on 2023/12/15.
+//  Copyright © 2023 Kkonmo. All rights reserved.
+//
+
+import Foundation
+
+public protocol RequestResponsable: Requestable, Responsable {}
+
+public class Endpoint<R>: RequestResponsable {
+    public typealias Response = R
+    
+    public var baseURL: String
+    public var path: String
+    public var method: HttpMethod
+    
+    public var queryPrameters: Encodable?
+    public var bodyParameters: Encodable?
+    public var headers: [String : String]?
+    public var sampleData: Data?
+    
+    public init(
+        baseURL: String,
+        path: String = "",
+        method: HttpMethod = .get,
+        queryPrameters: Encodable? = nil,
+        bodyParameters: Encodable? = nil,
+        headers: [String: String]? = [:],
+        sampleData: Data? = nil
+    ) {
+        self.baseURL = baseURL
+        self.path = path
+        self.method = method
+        self.queryPrameters = queryPrameters
+        self.bodyParameters = bodyParameters
+        self.headers = headers
+        self.sampleData = sampleData
+    }
+}

80ddc34a831478a3f780d95d0288004977b5fa5b
simyo <hj.sim@muzlive.com>
2023-12-15 23:34:53 +0900

    [Feat] Network: 모듈 샘플 코드 작성

--- This-Station-Is/Projects/Modules/Network/Samples/Sources/AppDelegate.swift
+++ This-Station-Is/Projects/Modules/Network/Samples/Sources/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Foundation
 import UIKit
+import Network
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -10,10 +11,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         vc.title = "Network"
         vc.view.backgroundColor = .systemYellow
         
+        APIServiceManager().request(with: getTerms()) { result in
+            switch result {
+            case .success(let success):
+                print("### success is \(success)")
+            case .failure(let failure):
+                print("### failure is \(failure)")
+            }
+        }
+        
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.rootViewController = UINavigationController(rootViewController: vc)
         self.window?.makeKeyAndVisible()
         
         return true
+    }
+}
+
+extension AppDelegate {
+    func getTerms() -> Endpoint<TempResponse> {
+        return Endpoint(
+            baseURL: "http://ec2-3-37-127-228.ap-northeast-2.compute.amazonaws.com",
+            path: "api/v1/terms/TERMS01"
+        )
+    }
+}
+
+struct TempResponse: Decodable {
+    let terms: String
+    let isRequired: Bool
+    let downloadableUrl: String
+
+    private enum CodingKeys: String, CodingKey {
+        case terms
+        case isRequired
+        case downloadableUrl
     }
 }

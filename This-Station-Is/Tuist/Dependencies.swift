1a1e44125b6cb249c84c73cd5c25c80dfaf18da8
simyo <hj.sim@muzlive.com>
2023-12-20 21:03:14 +0900

    [Fix] Tuist: Dependencies 문법 수정

--- This-Station-Is/Tuist/Dependencies.swift
+++ This-Station-Is/Tuist/Dependencies.swift
@@ -10,10 +10,12 @@ import ProjectDescriptionHelpers
 import UtilityPlugin
 
 let dependencis = Dependencies(
-    swiftPackageManager: [
-        .remote(url: "https://github.com/devxoul/Then", requirement: .upToNextMajor(from: "2")),
-        .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMajor(from: "5.0.1")),
-        .remote(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", requirement: .upToNextMajor(from: "4.0.0"))
-    ],
+    swiftPackageManager: SwiftPackageManagerDependencies.init(
+        [
+            .remote(url: "https://github.com/devxoul/Then", requirement: .upToNextMajor(from: "2")),
+            .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMajor(from: "5.0.1")),
+            .remote(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", requirement: .upToNextMajor(from: "4.0.0"))
+        ]
+    ),
     platforms: [.iOS]
 )

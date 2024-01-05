23de6a7d12d404a3e0ceef0880f976bf0d59becd
simyo <hj.sim@muzlive.com>
2024-01-05 18:39:44 +0900

    [Feat] 지하철 호선 정보 공통으로 사용할 수 있게 싱글턴 패턴 제작

--- This-Station-Is/Projects/Modules/CommonProtocol/Sources/CommonProtocol.swift
+++ This-Station-Is/Projects/Modules/CommonProtocol/Sources/CommonProtocol.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Network
 
 public struct CommonProtocol {
     public init() {}
@@ -6,4 +7,39 @@ public struct CommonProtocol {
 
 public protocol Coordinator {
     func start()
+}
+
+
+public class DataManager {
+    public static let shared = DataManager()
+    
+    public var lineInfos: [Line] = []
+    
+    private init() {}
+    
+    public func getSubwayLine(completion: @escaping (() -> ())) {
+        let endpoint = Endpoint<ResponseWrapper<SubwayLinesResponse>> (
+            path: "api/v1/subway/lines"
+        )
+        
+        APIServiceManager().request(with: endpoint) { result in
+            switch result {
+            case .success(let success):
+                self.lineInfos = success.data.lines
+                completion()
+            case .failure(let failure):
+                print("### failure is \(failure)")
+            }
+        }
+    }
+    
+    struct SubwayLinesResponse: Decodable {
+        let lines: [Line]
+    }
+
+    public struct Line: Decodable {
+        public let id: Int
+        public let name: String
+        public let colorCode: String
+    }
 }

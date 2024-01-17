b4e3c1e8f3413990d12d35be45a2c50d3226e3ce
simyo <hj.sim@muzlive.com>
2024-01-17 18:29:07 +0900

    [Feat] Dependency: Lottie 라이브러리 추가

--- This-Station-Is/Tuist/Dependencies.swift
+++ This-Station-Is/Tuist/Dependencies.swift
@@ -15,7 +15,8 @@ let dependencis = Dependencies(
             .remote(url: "https://github.com/devxoul/Then", requirement: .upToNextMajor(from: "2")),
             .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMajor(from: "5.0.1")),
             .remote(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", requirement: .upToNextMajor(from: "4.0.0")),
-            .remote(url: "https://github.com/hackiftekhar/IQKeyboardManager.git", requirement: .upToNextMajor(from: "6.5.7"))
+            .remote(url: "https://github.com/hackiftekhar/IQKeyboardManager.git", requirement: .upToNextMajor(from: "6.5.7")),
+            .remote(url: "https://github.com/airbnb/lottie-spm.git", requirement: .upToNextMajor(from: "4.3.4"))
         ]
     ),
     platforms: [.iOS]

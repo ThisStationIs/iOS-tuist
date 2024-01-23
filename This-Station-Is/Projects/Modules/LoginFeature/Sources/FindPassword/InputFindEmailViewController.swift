f73086aef0e7acfb48c90145cbd3b44b2cfeefdd
simyo <hj.sim@muzlive.com>
2024-01-23 20:58:44 +0900

    [Fix] Login: 이메일 입력 화면 터치시 키보드 내려가는 로직 추가

--- This-Station-Is/Projects/Modules/LoginFeature/Sources/FindPassword/InputFindEmailViewController.swift
+++ This-Station-Is/Projects/Modules/LoginFeature/Sources/FindPassword/InputFindEmailViewController.swift
@@ -38,6 +38,7 @@ public class InputFindEmailViewController: UIViewController {
         setLayout()
         setDelegate()
         setBinding()
+        hideKeyboardWhenTappedAround()
     }
 }
 
--- This-Station-Is/Projects/Modules/LoginFeature/Sources/SignUp/InputEmailViewController.swift
+++ This-Station-Is/Projects/Modules/LoginFeature/Sources/SignUp/InputEmailViewController.swift
@@ -72,6 +72,7 @@ public class InputEmailViewController: UIViewController {
         setDelegate()
         setBinding()
         setBottomSheet()
+        hideKeyboardWhenTappedAround()
     }
 }
 
